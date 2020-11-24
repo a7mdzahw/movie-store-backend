c23d9472cfec73f678421b73e91191bb3545645b
a7mdzahw <a7mdzahw@gmail.com>
2020-11-24 16:46:59 -0500

    Allow Cors

--- startup/routes.js
+++ startup/routes.js
@@ -4,10 +4,12 @@ const users = require("../routes/users");
 const movies = require("../routes/movies");
 const genres = require("../routes/genres");
 const home = require("../routes/home");
+const cors = require("cors")
 const error = require("../middlewares/error");
 
 module.exports = function (app) {
   app.use(express.json());
+  app.use(cors())
   app.use("/", home);
   app.use("/api/auth", auth);
   app.use("/api/users", users);
