230794d0432fa2e2695820108abede04a863af08
a7mdzahw <a7mdzahw@gmail.com>
2020-11-26 04:05:22 -0500

    fix delete route

--- routes/todos.js
+++ routes/todos.js
@@ -44,7 +44,7 @@ router.put("/:id", auth, async (req, res) => {
 router.delete("/:id", auth, async (req, res) => {
   try {
     const todo = await ToDo.find({ userId: req.user._id, _id: req.params.id });
-    if (!todo) {
+    if (!todo.length) {
       return res.status(404).send("No Such ToDo or Deleted");
     }
     await ToDo.deleteOne({ _id: req.params.id });
